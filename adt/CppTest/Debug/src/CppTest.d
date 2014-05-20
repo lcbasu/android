18c7f7b5df5396304bc3182edbb1808fa8f58165
Lokesh Basu <lokesh.basu@gmail.com>
2014-05-20 16:11:37 +0530

    testing standalone C++ application build

---
+++ adt/CppTest/Debug/src/CppTest.d
@@ -0,0 +1,1 @@
+src/CppTest.d: ../src/CppTest.cpp
---
+++ adt/CppTest/src/CppTest.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : CppTest.cpp
+// Author      : Lokesh Basu
+// Version     :
+// Copyright   : IIT Roorkee
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "C++ Test!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
