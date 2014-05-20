b3bb8f58669067774138ffa53a85f1af8babfb37
Lokesh Basu <lokesh.basu@gmail.com>
2014-05-20 16:04:54 +0530

    testing standalone java application build

---
+++ adt/JavaTest/src/com/basu/java/javatest/JavaTest.java
@@ -0,0 +1,14 @@
+package com.basu.java.javatest;
+
+public class JavaTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Java Test!");
+
+	}
+
+}
