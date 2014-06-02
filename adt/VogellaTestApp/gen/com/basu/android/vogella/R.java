053a60fff3aed463c1833c7eabd5b204af43f058
Lokesh Basu <lokesh.basu@gmail.com>
2014-06-02 07:08:21 +0530

    finish up email activity.

--- adt/VogellaTestApp/gen/com/basu/android/vogella/R.java
+++ adt/VogellaTestApp/gen/com/basu/android/vogella/R.java
@@ -8,7 +8,13 @@
 package com.basu.android.vogella;
 
 public final class R {
+    public static final class array {
+        public static final int operationsystems=0x7f070000;
+    }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int red=0x7f080000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -19,23 +25,25 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int mymargin=0x7f040002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f050001;
+        public static final int button1=0x7f050000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f0a0000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /** 
@@ -59,10 +67,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f090000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f090001;
     }
 }
