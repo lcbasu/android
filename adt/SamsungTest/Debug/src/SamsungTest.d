2cfe19129ee904744ddfc43acb8de85a9bce4649
Lokesh Basu <lokesh.basu@gmail.com>
2014-07-28 21:31:23 +0530

    just testing

---
+++ adt/SamsungTest/Debug/src/SamsungTest.d
@@ -0,0 +1,1 @@
+src/SamsungTest.d: ../src/SamsungTest.cpp
---
+++ adt/SamsungTest/src/SamsungTest.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : SamsungTest.cpp
+// Author      : Lokesh Basu
+// Version     :
+// Copyright   : Samsung Research Institute Bangalore, India
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[] = {1,2,3};
+	cout << arr[2] << endl;
+	return 0;
+}
