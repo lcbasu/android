30a8bb22dc9f730165327d6ec3c673f3629daab4
Lokesh Basu <lokesh.basu@gmail.com>
2014-09-10 21:52:47 +0530

    creates a sphere from triangles

--- adt/OpenGLES20Activity/src/com/example/android/opengl/MyGLRenderer.java
+++ adt/OpenGLES20Activity/src/com/example/android/opengl/MyGLRenderer.java
@@ -37,6 +37,7 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     private static final String TAG = "MyGLRenderer";
     private Triangle mTriangle;
     private Square   mSquare;
+    private Sphere mSphere;
 
     // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
     private final float[] mMVPMatrix = new float[16];
@@ -54,6 +55,7 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
 
         mTriangle = new Triangle();
         mSquare   = new Square();
+        mSphere = new Sphere();
     }
 
     @Override
@@ -88,6 +90,10 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
 
         // Draw triangle
         mTriangle.draw(scratch);
+        
+        //Draw Sphere
+        mSphere.draw(scratch, 0.5);
+        
     }
 
     @Override
