2d90e9aa0dd50bc86471967f0947bd08a980132a
Lokesh Basu <lokesh.basu@gmail.com>
2014-10-15 18:43:47 +0530

    adds scroll view

--- adt/Hunt2/gen/com/basu/hunt2/R.java
+++ adt/Hunt2/gen/com/basu/hunt2/R.java
@@ -28,9 +28,11 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int dummy_button=0x7f070002;
         public static final int fullscreen_content=0x7f070000;
         public static final int fullscreen_content_controls=0x7f070001;
+        public static final int textView1=0x7f070003;
     }
     public static final class layout {
         public static final int activity_fullscreen=0x7f030000;
+        public static final int scroll_view=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
