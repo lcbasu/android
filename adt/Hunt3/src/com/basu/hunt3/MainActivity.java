fc782924449de1321d7b341616d5f484237dff72
Lokesh Basu <lokesh.basu@gmail.com>
2014-10-15 19:51:37 +0530

    adds button interaction

--- adt/Hunt3/src/com/basu/hunt3/MainActivity.java
+++ adt/Hunt3/src/com/basu/hunt3/MainActivity.java
@@ -3,6 +3,8 @@ package com.basu.hunt3;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,12 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	
+	public void onClick(View view) {
+		
+		Toast.makeText(this, "Button 1 pressed", Toast.LENGTH_LONG).show();
 	}
 
 }
