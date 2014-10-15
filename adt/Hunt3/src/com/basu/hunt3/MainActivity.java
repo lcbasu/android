a9db86f9973d45696c7c139b76783b35b045cea0
Lokesh Basu <lokesh.basu@gmail.com>
2014-10-15 20:12:45 +0530

    implementing the swap of orientation

--- adt/Hunt3/src/com/basu/hunt3/MainActivity.java
+++ adt/Hunt3/src/com/basu/hunt3/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -13,6 +15,28 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//Radio group
+		
+		RadioGroup group1 = (RadioGroup)findViewById(R.id.orientation);
+		group1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				
+				switch (checkedId) {
+				case R.id.horizontal:
+					group.setOrientation(LinearLayout.HORIZONTAL);
+					break;			
+				case R.id.vertical:
+					group.setOrientation(LinearLayout.VERTICAL);
+					break;
+				}
+				
+			}
+		});
+		
+		
 	}
 
 	@Override
@@ -26,8 +50,8 @@ public class MainActivity extends Activity {
 	public void onClick(View view) {
 		
 		EditText text = (EditText) findViewById(R.id.main_input);
-		
 		Toast.makeText(this, text.getText().toString(), Toast.LENGTH_LONG).show();
+		
 	}
 
 }
