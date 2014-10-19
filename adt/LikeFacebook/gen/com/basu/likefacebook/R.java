52aa1987d04399f7d7a184a34200bfbdcf918f45
Lokesh Basu <lokesh.basu@gmail.com>
2014-10-19 11:08:27 +0530

    setup main activity

--- adt/LikeFacebook/gen/com/basu/likefacebook/R.java
+++ adt/LikeFacebook/gen/com/basu/likefacebook/R.java
@@ -10,6 +10,14 @@ package com.basu.likefacebook;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int feed_bg=0x7f040001;
+        public static final int feed_item_bg=0x7f040002;
+        public static final int feed_item_border=0x7f040003;
+        public static final int link=0x7f040004;
+        public static final int timestamp=0x7f040005;
+        public static final int white=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,25 +25,45 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int feed_item_border_width=0x7f05000c;
+        public static final int feed_item_corner_radius=0x7f05000b;
+        public static final int feed_item_margin=0x7f050002;
+        public static final int feed_item_padding_left_right=0x7f050004;
+        public static final int feed_item_padding_top_bottom=0x7f050003;
+        public static final int feed_item_profile_info_padd=0x7f050006;
+        public static final int feed_item_profile_name=0x7f050007;
+        public static final int feed_item_profile_pic=0x7f050005;
+        public static final int feed_item_status_pad_left_right=0x7f050009;
+        public static final int feed_item_status_pad_top=0x7f05000a;
+        public static final int feed_item_timestamp=0x7f050008;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bg_parent_rounded_corner=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090007;
+        public static final int feedImage1=0x7f090006;
+        public static final int list=0x7f090000;
+        public static final int name=0x7f090002;
+        public static final int profilePic=0x7f090001;
+        public static final int timestamp=0x7f090003;
+        public static final int txtStatusMsg=0x7f090004;
+        public static final int txtUrl=0x7f090005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int feed_item=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /** 
@@ -59,10 +87,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
