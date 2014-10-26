3a84f74eda0d47adbed834fca8aa4197d23e00d7
Lokesh Basu <lokesh.basu@gmail.com>
2014-10-26 13:52:11 +0530

    adds application id and client key

--- adt/DispatchActivity/src/com/parse/anywall/Application.java
+++ adt/DispatchActivity/src/com/parse/anywall/Application.java
@@ -33,8 +33,8 @@ public class Application extends android.app.Application {
     super.onCreate();
 
     ParseObject.registerSubclass(AnywallPost.class);
-    Parse.initialize(this, "YOUR_PARSE_APPLICATION_ID",
-        "YOUR_PARSE_CLIENT_KEY");
+    Parse.initialize(this, "IVjGwmLAIISS37V2w1qDHF11Nyh13N3EylOrW8Sq",
+        "tINiWqMnKrqL5UIZGNMcFY6AngwNkMvfULHLIkye");
 
     preferences = getSharedPreferences("com.parse.anywall", Context.MODE_PRIVATE);
 
